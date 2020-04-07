99d7b9c9bae4059663ae4c20e589577de11d721b
ayush-704 <61905568+ayush-704@users.noreply.github.com>
2020-04-07 04:44:45 -0700

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,16 @@
+n=int(input("Upto how many numbers u wanna input :"))
+f=0
+s=1
+if n==1:
+    print(0)
+elif n==2:
+    print(0,1)
+elif n>2:
+    print(0,1)
+    for x in range(n-2):
+        sum=f+s
+        print(sum)
+        f=s
+        s=sum
+else:
+        print('invalid input')
